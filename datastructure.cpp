9d6adaae1e7bc95fa9e909851af0d57c88b2edff
cjpthree <cjpthree@126.com>
2014-05-30 23:22:21 +0800

    test

--- datastructure.cpp
+++ datastructure.cpp
@@ -8,124 +8,124 @@
 int _tmain(int argc, _TCHAR* argv[])
 {
     algo1_1_main();
-    algo1_2_main();
-    f_main();
-    main_1_1();
-
-    ALGO2_1_main();
-    ALGO2_2_main();
-    ALGO2_3_main();
-    ALGO2_4_main();
-    Algo2_5_main();
-    Algo2_6_main();
-    Algo2_7_main();
-    Algo2_8_main();
-    Algo2_9_main();
-    Algo2_10_main();
-    Algo2_11_main();
-    Algo2_12_main();
-    Algo2_13_main();
-    main_2_1();
-    main_2_2();
-    main_2_4();
-    main_2_5();
-    main_2_6();
-    main_2_7();
-    main_2_8();
-    main_2_9();
-    main_2_31();
-    main_2_32();
-
-    ALGO3_1_main();
-    ALGO3_2_main();
-    ALGO3_3_main();
-    ALGO3_4_main();
-    ALGO3_5_main();
-    ALGO3_6_main();
-    ALGO3_7_main();
-    ALGO3_8_main();
-    ALGO3_9_main();
-    ALGO3_10_main();
-    ALGO3_11_main();
-    ALGO3_12_main();
-    main_3_1();
-    main_3_2();
-    main_3_3();
-    main_3_4();
-    main_3_5();
-
-    ALGO4_1_main();
-    ALGO4_2_main();
-    ALGO4_3_main();
-    ALGO4_4_main();
-    ALGO4_5_main();
-    main_4_1();
-    main_4_2();
-    main_4_3();
-
-    ALGO5_1_main();
-    main_5_1();
-    main_5_2();
-    main_5_3();
-    main_5_4();
-    main_5_51();
-    main_5_52();
-    main_5_6();
-
-    ALGO6_1_main();
-    ALGO6_2_main();
-    main_6_1();
-    main_6_2();
-    main_6_3();
-    main_6_4();
-    main_6_5();
-    main_6_6();
-
-    Algo7_1_main();
-    Algo7_2_main();
-    Algo7_3_main();
-    Algo7_4_main();
-    Algo7_5_main();
-    Algo7_6_main();
-    Algo7_7_main();
-    main_7_1();
-    main_7_2();
-    main_7_3();
-    main_7_4();
-
-    algo8_1_main();
-    algo8_2_main();
-    algo8_3_main();
-
-    Algo9_1_main();
-    Algo9_2_main();
-    Algo9_3_main();
-    Algo9_4_main();
-    Algo9_5_main();
-    Algo9_6_main();
-    Algo9_7_main();
-    Algo9_8_main();
-    Algo9_9_main();
-
-    Algo10_1_main();
-    Algo10_2_main();
-    Algo10_3_main();
-    Algo10_4_main();
-    Algo10_5_main();
-    Algo10_6_main();
-    Algo10_7_main();
-    Algo10_8_main();
-    Algo10_9_main();
-    Algo10_10_main();
-    Algo10_11_main();
-
-    Algo11_1_main();
-    Algo11_2_main();
-    Algo11_3_main();
-
-    Algo12_1_main();
-
-   list_main();
+//    algo1_2_main();
+//    f_main();
+//    main_1_1();
+//
+//    ALGO2_1_main();
+//    ALGO2_2_main();
+//    ALGO2_3_main();
+//    ALGO2_4_main();
+//    Algo2_5_main();
+//    Algo2_6_main();
+//    Algo2_7_main();
+//    Algo2_8_main();
+//    Algo2_9_main();
+//    Algo2_10_main();
+//    Algo2_11_main();
+//    Algo2_12_main();
+//    Algo2_13_main();
+//    main_2_1();
+//    main_2_2();
+//    main_2_4();
+//    main_2_5();
+//    main_2_6();
+//    main_2_7();
+//    main_2_8();
+//    main_2_9();
+//    main_2_31();
+//    main_2_32();
+//
+//    ALGO3_1_main();
+//    ALGO3_2_main();
+//    ALGO3_3_main();
+//    ALGO3_4_main();
+//    ALGO3_5_main();
+//    ALGO3_6_main();
+//    ALGO3_7_main();
+//    ALGO3_8_main();
+//    ALGO3_9_main();
+//    ALGO3_10_main();
+//    ALGO3_11_main();
+//    ALGO3_12_main();
+//    main_3_1();
+//    main_3_2();
+//    main_3_3();
+//    main_3_4();
+//    main_3_5();
+//
+//    ALGO4_1_main();
+//    ALGO4_2_main();
+//    ALGO4_3_main();
+//    ALGO4_4_main();
+//    ALGO4_5_main();
+//    main_4_1();
+//    main_4_2();
+//    main_4_3();
+//
+//    ALGO5_1_main();
+//    main_5_1();
+//    main_5_2();
+//    main_5_3();
+//    main_5_4();
+//    main_5_51();
+//    main_5_52();
+//    main_5_6();
+//
+//    ALGO6_1_main();
+//    ALGO6_2_main();
+//    main_6_1();
+//    main_6_2();
+//    main_6_3();
+//    main_6_4();
+//    main_6_5();
+//    main_6_6();
+//
+//    Algo7_1_main();
+//    Algo7_2_main();
+//    Algo7_3_main();
+//    Algo7_4_main();
+//    Algo7_5_main();
+//    Algo7_6_main();
+//    Algo7_7_main();
+//    main_7_1();
+//    main_7_2();
+//    main_7_3();
+//    main_7_4();
+//
+//    algo8_1_main();
+//    algo8_2_main();
+//    algo8_3_main();
+//
+//    Algo9_1_main();
+//    Algo9_2_main();
+//    Algo9_3_main();
+//    Algo9_4_main();
+//    Algo9_5_main();
+//    Algo9_6_main();
+//    Algo9_7_main();
+//    Algo9_8_main();
+//    Algo9_9_main();
+//
+//    Algo10_1_main();
+//    Algo10_2_main();
+//    Algo10_3_main();
+//    Algo10_4_main();
+//    Algo10_5_main();
+//    Algo10_6_main();
+//    Algo10_7_main();
+//    Algo10_8_main();
+//    Algo10_9_main();
+//    Algo10_10_main();
+//    Algo10_11_main();
+//
+//    Algo11_1_main();
+//    Algo11_2_main();
+//    Algo11_3_main();
+//
+//    Algo12_1_main();
+//
+//   list_main();
 
 	return 0;
 }
